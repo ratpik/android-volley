4693138d527398e4302aa3d3080ce321274c6d5f
ratpik <pratikmandrekar@gmail.com>
2014-08-26 16:27:44 +0530

    Modified Hurlstack to report 401 as 401 instead of Network Error

--- src/com/android/volley/toolbox/HurlStack.java
+++ src/com/android/volley/toolbox/HurlStack.java
@@ -106,12 +106,28 @@ public class HurlStack implements HttpStack {
         setConnectionParametersForRequest(connection, request);
         // Initialize HttpResponse with data from the HttpURLConnection.
         ProtocolVersion protocolVersion = new ProtocolVersion("HTTP", 1, 1);
+        /**
         int responseCode = connection.getResponseCode();
         if (responseCode == -1) {
             // -1 is returned by getResponseCode() if the response code could not be retrieved.
             // Signal to the caller that something was wrong with the connection.
             throw new IOException("Could not retrieve response code from HttpUrlConnection.");
+        }**/
+        int responseCode = -1;
+        try {
+          responseCode = connection.getResponseCode();
+        } catch(IOException e)
+        {
+          responseCode = connection.getResponseCode();
         }
+
+        
+        if (responseCode == -1) {
+            // -1 is returned by getResponseCode() if the response code could not be retrieved.
+            // Signal to the caller that something was wrong with the connection.
+            throw new IOException("Could not retrieve response code from HttpUrlConnection.");
+        }
+
         StatusLine responseStatus = new BasicStatusLine(protocolVersion,
                 connection.getResponseCode(), connection.getResponseMessage());
         BasicHttpResponse response = new BasicHttpResponse(responseStatus);
